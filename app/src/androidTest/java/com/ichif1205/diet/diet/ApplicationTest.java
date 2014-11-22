4c4a9a7e47d447e2cc44af03c0a62733bb257f36
mofumofu3n <mofumofu3n@gmail.com>
2014-11-22 17:43:05 +0900

    first commit

---
+++ app/src/androidTest/java/com/ichif1205/diet/diet/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ichif1205.diet.diet;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
